f3250e797f1e2833996027fbc9d47ee7c6d3ddd4
GustavoGajdeczka <tavinhogustavog@gmail.com>
2020-06-12 03:01:37 -0300

    filtro por classificações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,36 @@ import data from './products/products.json';
 })
 export class AppComponent {
   products: any = data;
+  selectedDropdown: string;
+  selectedProducts: number;
+  hover: boolean;
+
   constructor() {
     console.log(this.products);
+    this.selectedDropdown = "Mais Populares";
+    this.selectedProducts = 0;
+  }
+
+  public sortType(filter: string) {
+    switch (filter) {
+      case 'lessPrice':
+        this.selectedDropdown = "Menos preço"
+        return this.products.sort((a, b) => { return a.price - b.price })
+        break;
+      case 'score':
+        this.selectedDropdown = "Mais Populares"
+        this.products.sort((a, b) => { return a.score - b.score })
+        return this.products.reverse((a, b) => { return a.score - b.score })
+        break;
+      case 'plusPrice':
+        this.selectedDropdown = "Maior preço"
+        this.products.sort((a, b) => { return a.price - b.price })
+        return this.products.reverse((a, b) => { return a.price - b.price })
+        break;
+      case 'alfabetic':
+        this.selectedDropdown = "Ordem alfabetica"
+        return this.products.sort((a, b) => a.name.localeCompare(b.name))
+        break;
+    }
   }
 }
